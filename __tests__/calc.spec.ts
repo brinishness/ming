3b170b11ff2f4b502349fb88484ef2fbd55db56f
tears <596231290@qq.com>
2022-09-02 17:50:29 +0800

    ci: 修复打包分支

--- __tests__/calc.spec.ts
+++ __tests__/calc.spec.ts
@@ -2,12 +2,15 @@
  * @Author: tears 596231290@qq.com
  * @Date: 2022-09-02 11:40:17
  * @LastEditors: tears 596231290@qq.com
- * @LastEditTime: 2022-09-02 11:45:15
+ * @LastEditTime: 2022-09-02 17:48:59
  * @FilePath: /css/__test__/calc.spec.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import calc from '../src'
+import calc, { mactt } from '../src'
 
 test('The calculation result should be 996.', () => {
   expect(calc(1024, 28)).toBe(996)
+})
+test('The calculation result should be 996.', () => {
+  expect(mactt(2)).toBe(4)
 })
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: tears 596231290@qq.com
  * @Date: 2022-09-02 11:05:49
  * @LastEditors: tears 596231290@qq.com
- * @LastEditTime: 2022-09-02 11:36:03
+ * @LastEditTime: 2022-09-02 17:47:52
  * @FilePath: /css/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,4 +11,7 @@ const calc = (a: number, b: number): number => {
 }
 // calc(1024, 28)
 calc(1024, 28)
+export const mactt = (a: number): number => {
+  return a * 2
+}
 export default calc
