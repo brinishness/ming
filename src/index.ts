8533629b46690253a51f2f6ab4bdb81428b65866
tears <596231290@qq.com>
2022-09-02 11:37:20 +0800

    ci: add eslint and commitlint

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: tears 596231290@qq.com
  * @Date: 2022-09-02 11:05:49
  * @LastEditors: tears 596231290@qq.com
- * @LastEditTime: 2022-09-02 11:32:00
+ * @LastEditTime: 2022-09-02 11:36:03
  * @FilePath: /css/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,4 +10,8 @@ const calc = (a: number, b: number): number => {
   return a - b
 }
 //  console.log(calc(1024, 28))
-console.log(calc(1024, 28))
+const calc = (a: number, b: number): void => {
+  console.log(a - b)
+}
+- // calc(1024, 28)
+calc(1024, 28)
