e9ed0a1f903cc18be5b23e30107ebf323f894053
tears <596231290@qq.com>
2022-09-03 10:16:24 +0800

    feat: 添加console

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: tears 596231290@qq.com
  * @Date: 2022-09-02 11:05:49
  * @LastEditors: tears 596231290@qq.com
- * @LastEditTime: 2022-09-02 17:47:52
+ * @LastEditTime: 2022-09-03 10:16:09
  * @FilePath: /css/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,4 +14,5 @@ calc(1024, 28)
 export const mactt = (a: number): number => {
   return a * 2
 }
+console.log(mactt(10))
 export default calc
