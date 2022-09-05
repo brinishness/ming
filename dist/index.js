39feeef3cf2d6467f7499b57e56cf15db790e255
tears <596231290@qq.com>
2022-09-05 11:10:53 +0800

    feat: 添加打包之后发布

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,15 @@
  * @Author: tears 596231290@qq.com
  * @Date: 2022-09-02 11:05:49
  * @LastEditors: tears 596231290@qq.com
- * @LastEditTime: 2022-09-02 17:47:52
+ * @LastEditTime: 2022-09-05 10:54:27
  * @FilePath: /css/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/**
+ *  计算
+ * @param a 减数
+ * @param b 被减数
+ * @returns 值
  */
 const calc = (a, b) => {
     return a - b;
@@ -14,5 +20,6 @@ calc(1024, 28);
 export const mactt = (a) => {
     return a * 2;
 };
+console.log(mactt(10));
 export default calc;
 //# sourceMappingURL=index.js.map
--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,3 +1,9 @@
+/**
+ *  计算
+ * @param a 减数
+ * @param b 被减数
+ * @returns 值
+ */
 declare const calc: (a: number, b: number) => number;
 export declare const mactt: (a: number) => number;
 export default calc;
